203f83723620e4cec4b375cb23275a9766f4e81c
Kivoie <mindarc14@gmail.com>
2020-05-29 11:34:39 -0400

    edited the script to include explicitly upload saves and not changes made to this script, which will be done manually

--- bluereflection.sh
+++ bluereflection.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd "SAVEDATA"
+cd "C:\Users\danny\Documents\KoeiTecmo\BLUE REFLECTION\SAVEDATA"
 printf "===== Checking local repository status =====\n"
 git status
 printf "Pulling from BLUE REFLECTION repository to " && pwd
 printf "\n===== Starting pull sequence =====\n"
 git pull origin master
 printf "\n===== Starting push sequence =====\n"
-git add .
+git add *DAT*
 git commit -m "Saves updated"
 git push origin master
 printf "\n*------------------*"
